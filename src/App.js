721efed69004f3c29b93b10be24927f87c9c5cdd
Andrew Lohman <andrewjlohman@gmail.com>
2023-03-24 09:22:25 -0600

    initiall Commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+// import logo from './logo.svg';
+
+import {useState} from "react";
+import './App.css';
+import Square from './Square';
+
+function App() {
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState(true);
+  // const propVar = "x"
+
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""])
+    setPlayer(true)
+  }
+
+const calculateWinner = (arr) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for(let i = 0; i <lines.length; i++){
+      const [a,b,c] = lines[i];
+      if(arr[a] && arr[a] === arr[b] && arr[a] === arr[c]){
+        return `${arr[a]} Won!`;
+      }
+    }
+    return "Who will win?";
+}
+
+
+
+  return (
+    <div className="App">
+      <div className="container">
+        {squares.map((value, index) => {
+          return (
+            <Square 
+            squares={squares}
+            setSquares={setSquares}
+            player={player}
+            setPlayer={setPlayer}
+            squareValue={value}
+            index={index}
+            />
+            );
+          })}
+        <button onClick={handleClick}>Reset</button>
+      </div>
+      <span>{calculateWinner(squares)}</span>
+    </div>
+  );
+  
+}
+
+export default App;
